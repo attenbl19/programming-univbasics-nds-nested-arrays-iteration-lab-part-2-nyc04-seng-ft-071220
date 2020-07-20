6a681c88e03434c97a528f0cd5391d542479ca9a
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-20 15:08:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,17 +4,17 @@ def find_min_in_nested_arrays(src)
 new_array_outer = []
 row_index = 0
 
-while row_index < src.count do
-  element_index = 0
-  new_array_inner = []
-  lowest_value = 100
-  if src[row_index][element_index] < lowest_value
-    lowest_value = [row_index][element_index]
+  while row_index < src.count do
+    element_index = 0
+    new_array_inner = []
+    lowest_value = 100
+      if src[row_index][element_index] < lowest_value
+        lowest_value = [row_index][element_index]
+      end
+     element_index +=1
+   end
+   new_array_outer << lowest_value
+   row_index +=1
   end
-  element_index +=1
-end
-  new_array_outer << lowest_value
-  row_index +=1
-end
-new_array_outer
+  new_array_outer
 end
